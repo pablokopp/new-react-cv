fcdef7b2726a5d9f766d5c142d2f99ed82a99497
pablokopp <84154261+pablokopp@users.noreply.github.com>
2021-11-23 23:56:13 -0300

    2nd commit

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -2,7 +2,6 @@ import React from "react";
 // import Title from "../components/Title";
 import CuteLink from "../components/CuteLink";
 import Image from "../components/Image";
-import img from "../data/image/footergif.gif";
 const Footer = () => {
   return (
     <div className="footer">
@@ -17,7 +16,10 @@ const Footer = () => {
         cuteText="LinkdIn"
         cnCute="linkedin"
       />
-      <Image cnImg="footer" imgSrc={img} />
+      <Image
+        cnImg="footer"
+        imgSrc="https://firebasestorage.googleapis.com/v0/b/proyectocv-firegram.appspot.com/o/footergif.GIF?alt=media&token=64113346-4b41-47ee-8587-eb40f9a3b5a0"
+      />
       <CuteLink
         cuteHref="https://api.whatsapp.com/send?phone=5491169478092"
         cuteText="+54 9 1169478092"
