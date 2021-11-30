e9a28daa3f617013138f1831542af548348f7552
pablokopp <84154261+pablokopp@users.noreply.github.com>
2021-11-29 22:37:27 -0300

    Language / Context

--- src/containers/Study.js
+++ src/containers/Study.js
@@ -2,20 +2,33 @@ import React from "react";
 import Title from "../components/Title";
 import StudyExp from "../components/StudyExp";
 import dataS from "../data/dataS.json";
-
+import dataSen from "../data/dataSen.json";
+import { useContext } from "react/cjs/react.development";
+import { Language } from "../context/Language";
 const Study = () => {
+  const { english } = useContext(Language);
   return (
     <div className="study">
-      <Title cnTitle="study" text="MIS ESTUDIOS" />
-      {dataS.map((item) => (
-        <StudyExp
-          swhere={item.swhere}
-          swhen={item.swhen}
-          show={item.show}
-          sdesc={item.sdesc}
-          key={item.show}
-        />
-      ))}
+      <Title cnTitle="study" text={english ? "MY STUDIES" : "MIS ESTUDIOS"} />
+      {english
+        ? dataSen.map((item) => (
+            <StudyExp
+              swhere={item.swhere}
+              swhen={item.swhen}
+              show={item.show}
+              sdesc={item.sdesc}
+              key={item.show}
+            />
+          ))
+        : dataS.map((item) => (
+            <StudyExp
+              swhere={item.swhere}
+              swhen={item.swhen}
+              show={item.show}
+              sdesc={item.sdesc}
+              key={item.show}
+            />
+          ))}
     </div>
   );
 };
--- src/containers/Work.js
+++ src/containers/Work.js
@@ -1,21 +1,38 @@
 import React from "react";
+import { useContext } from "react/cjs/react.development";
 import Title from "../components/Title";
 import WorkExp from "../components/WorkExp";
+import { Language } from "../context/Language";
 import dataW from "../data/dataW.json";
+import dataWen from "../data/dataWen.json";
 
 const Work = () => {
+  const { english } = useContext(Language);
   return (
     <div className="work">
-      <Title cnTitle="work" text="EXPERIENCIA LABORAL" />
-      {dataW.map((item) => (
-        <WorkExp
-          jwhere={item.jwhere}
-          jwhen={item.jwhen}
-          jhow={item.jhow}
-          jdesc={item.jdesc}
-          key={item.jhow}
-        />
-      ))}
+      <Title
+        cnTitle="work"
+        text={english ? "WORK EXPERIENCE" : "EXPERIENCIA LABORAL"}
+      />
+      {english
+        ? dataWen.map((item) => (
+            <WorkExp
+              jwhere={item.jwhere}
+              jwhen={item.jwhen}
+              jhow={item.jhow}
+              jdesc={item.jdesc}
+              key={item.jhow}
+            />
+          ))
+        : dataW.map((item) => (
+            <WorkExp
+              jwhere={item.jwhere}
+              jwhen={item.jwhen}
+              jhow={item.jhow}
+              jdesc={item.jdesc}
+              key={item.jhow}
+            />
+          ))}
     </div>
   );
 };
