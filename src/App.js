fa271945516028a20094dc19ccfd35bc4d4c9c19
pablokopp <84154261+pablokopp@users.noreply.github.com>
2021-11-29 23:00:17 -0300

    Revert "Language / Context"
    This reverts commit 72829bb7179616d44c1e7b4b40cb2bbef4e567bb.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import Header from "./containers/Header";
 import WhoAmI from "./containers/WhoAmI";
 import Footer from "./containers/Footer";
 import WorkStudy from "./containers/WorkStudy";
-import { LanguageProvider } from "./context/Language";
-import LanguageChanger from "./components/LanguageChanger";
 
 function App() {
   return (
-    <LanguageProvider>
+    <div className="App">
       <Header />
-      <LanguageChanger />
       <WhoAmI />
       <WorkStudy />
       <Footer />
-    </LanguageProvider>
+    </div>
   );
 }
 
